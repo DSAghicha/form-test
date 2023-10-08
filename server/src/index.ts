17cd0dd92e8073aea1a4df87fc63ca0e2bb9f56c
Darshaan Aghicha <dsaghicha211@gmail.com>
2023-10-08 13:57:48 +0530

    Established Mongo Connection & add POST api endpoint for submitting forms.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,21 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import mongoose from 'mongoose'
+import { handleFormDataSubmit } from './service'
 const app = express()
 const PORT = 3001
 
 app.use(express.json({ limit: '50mb' }))
+app.post('/api/submitForm', async (req: Request, res: Response) => handleFormDataSubmit(req, res))
+
+mongoose
+	.connect('mongodb://localhost:27017/form-test')
+	.then(() => {
+		console.log('Connected successfully!')
+	})
+	.catch((err: any) => {
+		console.error('Connection failed', err)
+	})
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
 })
